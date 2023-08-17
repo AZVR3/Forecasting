2fbe98e0f5fdb513e853968da0bfe3fbae48edf6
AZVR3 <jongyul3213@gmail.com>
2023-08-17 20:47:02 +0900

    Update 10911001209-13481209

--- arima.py
+++ arima.py
@@ -4,12 +4,11 @@ import matplotlib.pyplot as plt
 # %matplotlib inline
 
 import pmdarima as pm
-from pmdarima.datasets import load_sunspots
 from pmdarima.model_selection import train_test_split
 print(f"Using pmdarima {pm.__version__}")
 # Using pmdarima 1.5.2
 
-df = pd.read_csv('south-korea-new-cases.csv') 
+df = pd.read_csv('south-korea-gathered-data.csv') 
 print(df.head())
 
 # Determine the size of the training set
@@ -17,51 +16,34 @@ dataSize = len(df)
 train_size = int(0.8 * dataSize)
 
 # Split the data into training and testing sets
-y_train = df['new_cases'][:train_size]
-y_test = df['new_cases'][train_size:]
+y_train = df['New_cases'][:train_size]
+y_test = df['New_cases'][train_size:]
 
 from pmdarima.utils import tsdisplay
-
-tsdisplay(y_train, lag_max=100)
-
-# ----------------------------------------------------------------
-
 from pmdarima.preprocessing import BoxCoxEndogTransformer
 
 y_train_bc, _ = BoxCoxEndogTransformer(lmbda2=1e-6).fit_transform(y_train)
 tsdisplay(y_train_bc, lag_max=100)
 
-# ----------------------------------------------------------------
-
 from scipy.stats import normaltest
 print(normaltest(y_train_bc)[1])
-## New cases
-# y_train: 5.941295417681244e-184
-# y_train_bc: 0.000991565134002783
-# y_train_log: 1.5127623302358342e-122
-
-## Total cases
-# y_train: 3.3936067176900496e-102
-# y_train_bc: 3.0522077068491763e-06
-# y_train_log: 2.273823980094518e-145
-
-# ----------------------------------------------------------------
 
 from pmdarima.pipeline import Pipeline
 
 fit2 = Pipeline([
     ('boxcox', BoxCoxEndogTransformer(lmbda2=1e-6)),
     ('arima', pm.AutoARIMA(trace=True,
-                           suppress_warnings=True,
-                           m=12))
+                         suppress_warnings=True,
+                         m=7, # set the seasonal period
+                         seasonal=True, # enable the seasonal component
+                         seasonal_test='ocsb', # use the OCSB test to determine D
+                         ))
 ])
 
 fit2.fit(y_train)
 print(fit2.summary())
 
 from sklearn.metrics import mean_squared_error as mse
-
-# ----------------------------------------------------------------
 
 def plot_forecasts(forecasts, title, figsize=(8, 12)):
     x = np.arange(y_train.shape[0] + forecasts.shape[0])
@@ -89,3 +71,25 @@ forecasts = fit2.predict(y_test.shape[0]) # Added this line
 
 # Plot the forecasts # Added this line
 plot_forecasts(forecasts, title='Box-Cox transformed ARIMA') # Added this line
+
+# Import the error functions
+from sklearn.metrics import mean_absolute_error as mae
+from sklearn.metrics import mean_absolute_percentage_error as mape
+from sklearn.metrics import median_absolute_error as mdae
+
+def smape(A, F):
+    return 100/len(A) * np.sum(2 * np.abs(F - A)/(np.abs(A) + np.abs(F)))
+
+# Calculate the errors
+mae_value = mae(y_test, forecasts)
+mdape_value = mdae(y_test, forecasts) / np.median(y_test)
+smape_value = smape(y_test, forecasts)
+mape_value = mape(y_test, forecasts)
+rmse_value = np.sqrt(mse(y_test, forecasts))
+
+# Print the errors
+print(f'MAE: {mae_value:.3f}')
+print(f'MdAPE: {mdape_value:.3f}')
+print(f'SMAPE: {smape_value:.3f}')
+print(f'MAPE: {mape_value:.3f}')
+print(f'RMSE: {rmse_value:.3f}')
